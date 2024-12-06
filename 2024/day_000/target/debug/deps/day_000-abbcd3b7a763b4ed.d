36605b5e1b994011df4308ed6b13766b49b26f3c
TankmanDu70 <thomas.perraton@gmail.com>
2024-12-06 11:23:38 +0100

    save:

---
+++ 2024/day_000/target/debug/deps/day_000-abbcd3b7a763b4ed.d
@@ -0,0 +1,6 @@
+/home/thomas/src/perso/advent/2024/day_000/target/debug/deps/day_000-abbcd3b7a763b4ed: src/main.rs src/data.rs
+
+/home/thomas/src/perso/advent/2024/day_000/target/debug/deps/day_000-abbcd3b7a763b4ed.d: src/main.rs src/data.rs
+
+src/main.rs:
+src/data.rs:
